88d0c646088752a219cdcd89c834d50c996bf64e
Ozaki <o.zaki@sap.com>
2019-08-24 19:35:36 +0100

    Create Main.java
    Add project main app.

---
+++ src/study/oca/examples/Main.java
@@ -0,0 +1,14 @@
+package study.oca.examples;
+
+import study.oca.examples.ch_02.Example01;
+import study.oca.examples.ch_02.Example02;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // run any example here.
+        Example02 example02 = new Example02();
+        example02.runExample();
+
+    }
+}
