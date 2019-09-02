a4a1390e707d9f63ba6e63f26309aeccfbe6b5c8
Ozaki <o.zaki@sap.com>
2019-09-02 22:09:32 +0100

    Update Example01.java
    Adding Importants String methods examples

--- src/study/oca/examples/ch_03/Example01.java
+++ src/study/oca/examples/ch_03/Example01.java
@@ -52,26 +52,80 @@ public class Example01
         String name = "omar"; // it uses string pool.
         String name3= new String("Xxxx"); // it does not use string pool.
 
-        // => Important String Methods
-        /**
-         *  -> lenght() : it return the number of chars in the String.
+        /*
+         *  => Important String Methods
          */
+        System.out.println("Important String Methods");
+         //  -> lenght() : it return the number of chars in the String.
         String string = "animals";
         System.out.printf("lentth() method will return for %s = %d %n", string,string.length());
 
         // => charAt(): let you query the string to find out what character is at a specific index.
-        // => indexOf();
-        // => substring()
-        // => toLowerCase() and toUpperCase()
-        // => equals() and equalsIgnoreCase();
+        String string2= "animals"; // the index starts from 0
+        System.out.println(string2.charAt(0)); // this will return 0
+        System.out.println(string2.charAt(6)); // this will return s
+        //System.out.println(string2.charAt(7)); // throws exception it throws java.lang.StringIndexOutOfBoundsException
+
+        // => indexOf() it return the first inndex of the matched char, it works with one char or whole string as input. it can start from requested position.
+        // The following examples shows how it works:
+        String string3 = "animals";
+        System.out.println(string3.indexOf('a')); // it will return 0
+        System.out.println(string3.indexOf("al"));// it will return
+        System.out.println(string3.indexOf("a",4));
+        System.out.println(string3.indexOf("al",5)); // the method does not throw an exception if the match does not found. it returns -1;
+
+        // => substring() : The method looks for a chars in string. It returns parrts of the string. The first parameter is the index to start with for the returned string. as usual this is a zero-based index.
+        // There is an optional second parameter, which is the end index you want to stop at.
+        // Is the hardest one in the exam.
+        // beginIndex : start from it, it is included.
+        // endIndex: stop at it. It will not be included.
+        // Examples here:
+        String string4 = "animals";
+        System.out.println(string4.substring(3)); // return mals
+        System.out.println(string4.substring(string4.indexOf('m'))); // return mals
+        System.out.println(string4.substring(3,4)); // index 4 will not be included, it returns m
+        System.out.println(string4.substring(3,7)); // 7 is like saying that end of the string it will stop on it. so it will included the inded 7-1 which is the "s" end of the string.
+        System.out.println(string4.substring(3,3)); // return empty string
+//        System.out.println(string4.substring(3,2)); // throw exception
+//        System.out.println(string4.substring(3,8)); // throw exception
+
+        // => toLowerCase() and toUpperCase() Convert every lower char to upper char and vice versa.
+        // It has no effect on numbers.
+        // examples:
+        String string5 = "animals";
+        System.out.println(string5.toUpperCase());
+        System.out.println("Abc123".toLowerCase());
+
         // => startsWith() and endsWith()
-        // => contains()
-        // => replace()
-        // => trim()
+        // => equals() and equalsIgnoreCase():
+        // equals() checks whether two String objects contains exactly the same chars in the same order.
+        // equalsIgnoreCase() checks whether two String objects contains the same chars with the exception whtat it will convert the chars in needed.
+        // it return boolean.
+        // examples:
+        System.out.println("abc".equals("ABC")); // return false.
+        System.out.println("ABC".equals("ABC")); // return true.
+        System.out.println("abc".equalsIgnoreCase("ABC")); // return true.
 
+        // => startsWith() and endsWith() it checks if the string starts/ends with a provided string, it return boolean.
+        // examples:
+        System.out.println("abc".startsWith("a")); // true
+        System.out.println("Abc".startsWith("a")); // false
+        System.out.println("abc".endsWith("c")); // true
+        System.out.println("abs".endsWith("c")); // false
 
+        // => contains(): looks for matches in the String. The match can be anywhere in the String.
+        System.out.println("abc".contains("b")); // true
+        System.out.println("abc".contains("B")); // false
+        System.out.println("abc".indexOf('a') != -1 ); // cantains() method can be used instead of this.
 
+        // => replace() : does a simple search and replace one the string.
+        System.out.println("abcabc".replace('a', 'A')); // AbcAbc
+        System.out.println("abcabc".replace("ab", "A")); // AcAc
 
+        // => trim() it removes the white spaces from the beginning and end of the string.
+        // Whitespaces consists of spaces, \t (tab) ,\n (newline) and \r.
+        System.out.println("abc".trim()); // abc
+        System.out.println("\t   a b c \n \r ".trim());
     }
 
 }
